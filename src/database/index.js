0dacddbf93a03cd6eb6412d7969b7faa11fdc293
Lucas <lwsouza@outlook.com.br>
2020-09-08 10:49:43 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true);
 
-mongoose.connect('mongodb://admin:gestao2019@ds243607.mlab.com:43607/gestaovendas', { useNewUrlParser: true });
+mongoose.connect('mongodb://user:password@endereco/table', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
